9b67d3bc8054e0ed40a2f80098570f5ba0f71e54
davidiro <irofualadavid6@gmail.com>
2025-12-31 12:16:31 -0800

    added endpoint express for products

--- server.js
+++ server.js
@@ -61,3 +61,45 @@ app.post("/login", (req, res) => {
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
+
+//producs endpoint 
+db.run(`CREATE TABLE IF NOT EXISTS PRODUCTS (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    PRICE REAL NOT NULL,
+    Image TEXT NOT NULL
+)`);
+
+// Insert sample products (if empty)
+db.get("SELECT COUNT(*) AS count FROM products", (err, row) => {
+    if (row.count === 0) {
+        const sample = [
+            { name: "Latte", price: 3.5, image: "/images/cake image" },
+            { name: "Cappuccino", price: 4.0, image: "/images/coffeimg1" },
+            { name: "Espresso", price: 2.5, image: "/images/coffeimg2" },
+            
+        ];
+
+        const stmt = db.prepare("INSERT INTO products (name, price, image) VALUES (?, ?, ?)");
+        sample.forEach(p => stmt.run(p.name, p.price, p.image));
+        stmt.finalize();
+    }
+});
+
+app.get("/products", (req, res) => {
+    const page = Number(req.query.page) || 1;
+    const limit = 6;
+    const offset = (page - 1) * limit;
+
+
+  db.all(
+    "SELECT name, price, image FROM products LIMIT ? OFFSET ?",
+        [limit, offset],
+        (err, rows) => {
+            if (err) {
+                return res.status(500).json({ message: "Database error" });
+            }
+            res.json(rows);
+        }
+    );
+});
